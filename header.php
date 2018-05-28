0b34a72af438087b9fc199eb9f0157c18d388948
Avery Howell <avery.howell@beanstalkdigital.com>
2018-05-28 16:17:20 -0500

    fonts

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=11">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
+	<link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
