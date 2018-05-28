5e25eb211c0bed090fa78e73aaff44c919fd9bd3
Avery Howell <avery.howell@beanstalkdigital.com>
2018-05-28 12:08:00 -0500

    blog

--- template-parts/team-member/team_member-card.php
+++
@@ -1,9 +0,0 @@
-<?php $acf = get_fields(); ?>
-<a href="<?php the_permalink(); ?>" class="card --team">
-  <?php echo the_post_thumbnail( 'square' ); ?>
-  <div class="card__contents">
-    <h4 class="card__contents__title"><?php the_title(); ?></h4>
-    <div class="card__contents__position"><?php echo $acf["position"]; ?></div>
-    <small class="card__contents__learnMore u-uppercase">Learn More</small>
-  </div>
-</a>
